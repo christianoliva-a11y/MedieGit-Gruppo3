867c64b05d94b085722384795072c3c06bac81fe
christianoliva-a11y <christian.oliva@mondelliamaldi.edu.it>
2026-01-08 10:26:23 +0100

    Refactor image asset handling and preload function
    Updated image asset loading to use window properties and added a preload function.

--- engine/assets.js
+++ engine/assets.js
@@ -1,15 +1,30 @@
-const bg1Img = new Image();
-const playerImg = new Image();
-const ghoulImg = new Image();
-const knightImg = new Image();
-const swordImg = new Image();
-const axeImg = new Image();
-const arrowImg = new Image();
+// Assicurati che window.BASE_PATH sia definito (audio.js imposterà un valore)
+window.bg1Img = new Image();
+window.playerImg = new Image();
+window.ghoulImg = new Image();
+window.knightImg = new Image();
+window.swordImg = new Image();
+window.axeImg = new Image();
+window.arrowImg = new Image();
 
-bg1Img.src     = BASE_PATH + "img/bg.jpg";
-playerImg.src  = BASE_PATH + "img/player.jpg";
-ghoulImg.src   = BASE_PATH + "img/ghoul.jpg";
-knightImg.src  = BASE_PATH + "img/knight.jpg";
-swordImg.src   = BASE_PATH + "img/sword.jpg";
-axeImg.src     = BASE_PATH + "img/axe.jpg";
-arrowImg.src   = BASE_PATH + "img/arrow.jpg";
+var bp = window.BASE_PATH !== undefined ? window.BASE_PATH : "";
+
+window.bg1Img.src     = bp + "img/bg.jpg";
+window.playerImg.src  = bp + "img/player.jpg";
+window.ghoulImg.src   = bp + "img/ghoul.jpg";
+window.knightImg.src  = bp + "img/knight.jpg";
+window.swordImg.src   = bp + "img/sword.jpg";
+window.axeImg.src     = bp + "img/axe.jpg";
+window.arrowImg.src   = bp + "img/arrow.jpg";
+
+// Helper per preload assets (se il resto del codice lo usa)
+window.preloadAssets = async function() {
+    const imgs = [window.bg1Img, window.playerImg, window.ghoulImg, window.knightImg, window.swordImg, window.axeImg, window.arrowImg];
+    await Promise.all(imgs.map(img => {
+        return new Promise(resolve => {
+            if (img.complete) return resolve();
+            img.onload = () => resolve();
+            img.onerror = () => resolve(); // non bloccare se mancante
+        });
+    }));
+};
