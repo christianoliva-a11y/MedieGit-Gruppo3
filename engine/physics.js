a979482dafd1ee45b50924b6856469ca160714cd
christianoliva-a11y <christian.oliva@mondelliamaldi.edu.it>
2026-01-08 10:27:12 +0100

    Refactor applyPhysics function for window context

--- engine/physics.js
+++ engine/physics.js
@@ -1,13 +1,13 @@
-const GRAVITY=0.8,MOVE_SPEED=4,JUMP_FORCE=-16;
+const GRAVITY = 0.8, MOVE_SPEED = 4, JUMP_FORCE = -16;
 
-function applyPhysics(player,tiles){
-    if(Input.left) player.vx=-MOVE_SPEED;
-    else if(Input.right) player.vx=MOVE_SPEED;
-    else player.vx=0;
+window.applyPhysics = function(player, tiles) {
+    if (window.Input && window.Input.left) player.vx = -MOVE_SPEED;
+    else if (window.Input && window.Input.right) player.vx = MOVE_SPEED;
+    else player.vx = 0;
 
-    if(Input.jump && player.onGround){ player.vy=JUMP_FORCE; player.onGround=false; }
+    if (window.Input && window.Input.jump && player.onGround) { player.vy = JUMP_FORCE; player.onGround = false; }
 
-    player.vy+=GRAVITY;
-    player.x+=player.vx;
-    player.y+=player.vy;
-}
+    player.vy += GRAVITY;
+    player.x += player.vx;
+    player.y += player.vy;
+};
